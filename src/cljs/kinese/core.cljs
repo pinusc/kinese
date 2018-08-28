beba7cba3ec2d63b0c1358c7530b52700489990d
pinusc <giuseppe.stelluto@gmail.com>
2018-08-28 19:22:31 +0200

    Remove lingering code

--- src/cljs/kinese/core.cljs
+++ src/cljs/kinese/core.cljs
@@ -1,12 +1,10 @@
 (ns kinese.core
     (:require [reagent.core :as reagent :refer [atom]]
-              [kinese.textarea]
               [kinese.contextual-definitions :refer [contextual-definitions]]
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]
               [ajax.core :refer [GET]]
               [clojure.string :as string]))
-(def default-text "米歇尔·阿弗拉克（1910年－1989年）是一位叙利亚哲學家、社会学家和阿拉伯民族主义者。他的理论对复兴社会主义的发展及其政治运动产生了深远影响；他被部分复兴社会主义者视为复兴社会主义学说的首要创始人。他生前出版了一些著作，主要有《为了复兴》、《唯一的归宿之战》和《反对扭曲阿拉伯革命运动的斗争》等。阿弗拉克出生于叙利亚大马士革的一个中產階級家庭。")
 
 (defn get-random-text
   []
@@ -30,12 +28,10 @@
      [:h2.subtitle.is-3.is-inline "a better approach to chinese learning"]]]])
 
 (defn home-page []
-  (let [definition-div (reagent/atom (kinese.definition/construct-definition))]
-    (fn []
-      [:div
-       [header]
-       [:section.section 
-        [contextual-definitions default-tokens]]])))
+  [:div
+   [header]
+   [:section.section 
+    [contextual-definitions default-tokens]]])
 
 (defn about-page []
   [:div [:h2 "About kinese"]
