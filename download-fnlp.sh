9db87ca21f2ae00dac672c33f243a27ffba022be
pinusc <giuseppe.stelluto@gmail.com>
2018-09-01 00:35:18 +0200

    Make it more obvious for debug

--- download-fnlp.sh
+++ download-fnlp.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-[ ! -f "resources/fnlp-core-2.1.jar" ] && wget -P resources/ "https://repo1.maven.org/maven2/org/fnlp/fnlp-core/2.1/fnlp-core-2.1.jar"
+[ ! -f "resources/fnlp-core-2.1.jar" ] && wget -q -P resources/ "https://repo1.maven.org/maven2/org/fnlp/fnlp-core/2.1/fnlp-core-2.1.jar"
+
+echo "================================="
+echo ""
+echo ""
+echo "Downloaded fnlp-core-2.1.jar"
+echo ""
+echo ""
+echo "================================="
 
 mkdir -p models
 
-[ ! -f "models/dep.m" ] && wget -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/dep.m"
-[ ! -f "models/seg.m" ] && wget -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/seg.m"
-[ ! -f "models/dep.m" ] && wget -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/pos.m"
+[ ! -f "models/dep.m" ] && wget -q -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/dep.m"
+[ ! -f "models/seg.m" ] && wget -q -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/seg.m"
+[ ! -f "models/dep.m" ] && wget -q -P models/ "https://github.com/FudanNLP/fnlp/releases/download/v2.1/pos.m"
 
 exit 0 # if anything it will fail at compile or run time
