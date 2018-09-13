0a742ce3135a565057fe0f84e4d9e6a69d5fc1e2
pinusc <giuseppe.stelluto@gmail.com>
2018-09-13 03:10:12 +0200

    FIX LINGERING HREF INSTEAD OF DISPATCH

--- src/cljs/kinese/components.cljs
+++ src/cljs/kinese/components.cljs
@@ -1,10 +1,11 @@
-(ns kinese.components)
+(ns kinese.components
+  (:require [secretary.core :as secretary :include-macros true]))
 
 (defn navbar []
   [:nav.navbar.is-transparent.is-fixed-top
    [:div.navbar-brand
     [:a.navbar-item
-     {:href "/"}
+     {:on-click #(secretary/dispatch! "/")}
      [:img {:src "/logo.svg" :alt "Kinese: a smarter way to immerse yourself"}]]]
    [:div.navbar-menu
     [:div.navbar-end
